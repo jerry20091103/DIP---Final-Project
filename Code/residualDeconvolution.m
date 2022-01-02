e5924fe5a07282c018ad4a1fec5ee98f6a668e2a
Brady29655751 <k0862.kimo@gmail.com>
2022-01-03 03:07:13 +0800

    residual deconv

---
+++ Code/residualDeconvolution.m
@@ -0,0 +1,28 @@
+function I = residualDeconvolution(B, K, Nd, iter)
+    % dB = B - Nd conv K
+    dB = B - conv2(Nd, K, 'same');
+    
+    % calculate total gradient magnitude of gaussian pyramid.
+    L = 5;
+    gradientTotal = 0;
+    NdPyramid = Nd;
+    for i = 0 : L
+        NdPyramid = imgaussfilt(Nd, 0.5);
+        [Gmag Gdir] = imgradient(NdPyramid);
+        gradientTotal = gradientTotal + Gmag;
+        NdPyramid = imresize(NdPyramid, 0.5);
+    end
+
+    % residual deconv with de-ringing
+    dI = dB;
+    alpha = 0.2;
+    Ig = (1 - alpha) + alpha * gradientTotal;
+    for i = 1 : (iter - 1)
+        dI = Ig * deconvlucy(dI, K, 1);
+    end
+
+    % Do not multiply Ig in last iter.
+    dI = deconvlucy(dI, K, 1);
+
+    I = Nd + dI;
+end
